b7c3b4840cc998d681ec82e09bbd658574a29733
anuragkhode <anurag.k@moolya.com>
2015-08-09 12:51:21 +0530

    myProject

---
+++ src/test/java/com/test/AppTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test
+	public void Test1(){
+		System.out.println("This is test 1 ");
+	}
+	
+	@Test
+	public void Test2(){
+		System.out.println("This is test 2 ");
+	}
+
+
+}
